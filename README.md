13bfd19045168193a1e4d8ec3158c1636fed0ae7
willy060317 <willy031712345678@gmail.com>
2025-07-18 15:00:03 +0900

    new

---
+++ README.md
@@ -0,0 +1,2 @@
+# oz-backend-sazo
+Django REST API 프로젝트
--- config/settings/base.py
+++ config/settings/base.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'oz_backend_sazo',        # ← 본인이 만든 DB 이름
+        'USER': 'postgres',       # ← PostgreSQL 접속 유저
+        'PASSWORD': 'willy0317',   # ← 그 유저의 비번
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
